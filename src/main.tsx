33ae6888935a71c43073f5e738d76970383c17aa
Jerie See Villanueva <codestackjs007@gmail.com>
2024-11-05 18:32:41 -0800

    initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import 'bootstrap/dist/css/bootstrap.css'
+import { Button } from 'react-bootstrap'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
