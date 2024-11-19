364882a8ecd917d20cf77484339418ae76abba17
Jerie See Villanueva <codestackjs007@gmail.com>
2024-11-18 20:49:54 -0800

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import Calendar from './components/months/Calendar'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import MoodyChatComponent from './components/MoodyChatComponent'
 import LoginComponent from './components/LoginComponent'
+import January from './components/months/january'
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -16,7 +17,8 @@ function App() {
     <Routes>
       <Route path='/' element={<LoginComponent/>}/>
       <Route path='/AI' element={<MoodyChatComponent/>}/>
-      <Route path='/Calendar' element={<Calendar/>}/>
+      {/* <Route path='/Calendar' element={<Calendar/>}/> */}
+      <Route path='/Calendar' element={<January/>}/>
     </Routes>
     
   </BrowserRouter>
